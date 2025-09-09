64a219737286cbf840c07444dbdd187256a2dfa4
Gerg0Vagyok <fake@example.com>
2025-09-09 21:16:43 +0200

    progress

--- UndertaleModToolGtk/Localization.cs
+++ UndertaleModToolGtk/Localization.cs
@@ -1,13 +1,17 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Text.Json;
 using Gtk;
 
 class Localization {
 	public String Language;
 	public List<(Widget widget, String text)> LocalizedWidgets = new List<(Widget widget, String text)>();
+	private Dictionary<String, Dictionary<String, String>> LocalizationData;
 
-	public Localization(String lang = "English") {
-		Language = lang;
+	public Localization(String Lang = "English", String LocalizationFileName = "lang.json") {
+		Language = Lang;
+		LocalizationData = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, string>>>(File.ReadAllText(LocalizationFileName));
 	}
 
 	public void Add(Widget widget, String text) {
@@ -15,17 +19,20 @@ class Localization {
 	}
 
 	public void Update() {
-		foreach ((Widget widget, String text) WidgetEl in LocalizedWidgets) {
-			if (WidgetEl.widget.GetType().GetProperty("Text") != null) {
-				Console.WriteLine($"Text - {WidgetEl.widget.GetType()}");
-			} else if (WidgetEl.widget.GetType().GetProperty("Label") != null) {
-				Console.WriteLine($"Label - {WidgetEl.widget.GetType()}");
-			} else {
-				Console.WriteLine($"For type '{WidgetEl.widget.GetType()}' output text field not found!");
+		if (LocalizationData.ContainsKey(Language)) {
+			var LangData = LocalizationData[Language];
+			foreach ((Widget widget, String text) WidgetEl in LocalizedWidgets) {
+				if (LangData.ContainsKey(WidgetEl.text)) {
+					if (WidgetEl.widget.GetType().GetProperty("Text") != null) {
+						WidgetEl.widget.SetProperty("Text", new GLib.Value(LocalizationData[Language][WidgetEl.text]));
+						Console.WriteLine($"Text - {WidgetEl.widget.GetType()}");
+					} else if (WidgetEl.widget.GetType().GetProperty("Label") != null) {
+						Console.WriteLine($"Label - {WidgetEl.widget.GetType()}");
+					} else {
+						Console.WriteLine($"For type '{WidgetEl.widget.GetType()}' output text field not found!");
+					}
+				}
 			}
-			//if (WidgetEl.Text != null) {
-			//	Console.WriteLine("Its label");
-			//}
 		}
 	}
 }
